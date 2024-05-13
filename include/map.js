d69031346889f3270e982452b8263bb4e408a3fe
Muhammad Ali <engr.alikhan2001@gmail.com>
2024-05-13 11:37:50 +0500

    Tracer system for vehical

---
+++ include/map.js
@@ -0,0 +1,15 @@
+
+
+
+map.setCenter(new google.maps.LatLng(-34, 151));
+map.setCenter({lat: -34, lng: 151});  new google.maps.LatLng(-34, 151);
+
+  new google.maps.LatLng(-34, 151, true);
+  new google.maps.LatLng({lat: -34, lng: 151});
+  new google.maps.LatLng({lat: -34, lng: 151}, true);
+  
+  new google.maps.LatLng(new google.maps.LatLng(-34, 151));
+  new google.maps.LatLng(new google.maps.LatLng(-34, 151), true); 
+  
+  map.setCenter({lat: -34, lng: 151});
+  new google.maps.Marker({position: {lat: -34, lng: 151}, map: map});   
---
+++ src/main.cpp
@@ -0,0 +1,100 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+
+#define wifissid "CST WIFI"
+#define wifipssd "iotdevs123"
+#define led 2
+
+void handleOnroot();
+void handleOnNotFound();
+uint8_t H, T;
+WebServer Server(80);
+
+void setup() {
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifissid,wifipssd);
+  Serial.begin(9600);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+
+  Serial.print("\nConnected to ");
+  Serial.print(wifissid);
+  Serial.print(" with local ip ");
+  Serial.print(WiFi.localIP());
+
+  Server.begin();
+  Server.on("/", handleOnroot);
+
+  Server.onNotFound(handleOnNotFound);
+
+
+
+}
+
+void loop() {
+
+  Server.handleClient();
+  delay(500);
+
+}
+
+
+void handleOnroot() {
+  digitalWrite(led, 1);
+  char temp[400];
+
+  H = random();
+  T = random();
+
+  snprintf(temp, 400,
+  "<html>\
+  <head>\
+    <meta http-equiv='refresh' content='5'/>\
+    <title>ESP32 Demo</title>\
+    <style>\
+      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+    </style>\
+  </head>\
+  <body>\
+    <h1>L and H from ESP32!</h1>\
+    <p>H: %01d</p>\
+    <p>T: %01d</p>\
+  </body>\
+</html>",
+
+           H, T
+          );
+  Server.send(200, "text/html", temp);
+  digitalWrite(led, 0);
+}
+
+void handleOnNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found bro\n\n";
+  message += "URI: ";
+  message += Server.uri();
+  message += "\nMethod: ";
+  message += (Server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += Server.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < Server.args(); i++) {
+    message += " " + Server.argName(i) + ": " + Server.arg(i) + "\n";
+  }
+
+  Server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void returnfalse(){
+  
+  
+
+}
